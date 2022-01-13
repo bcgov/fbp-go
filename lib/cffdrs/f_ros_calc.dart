695e32d8cb8cb968d9cf7de25d64c32425b8699e
Strauss <Sybrand.Strauss@gov.bc.ca>
2022-01-13 07:09:40 -0800

    added licensing

--- lib/cffdrs/f_ros_calc.dart
+++ lib/cffdrs/f_ros_calc.dart
@@ -1,3 +1,29 @@
+/*
+Copyright 2021, 2022 Province of British Columbia.
+
+This file is part of FBP Go.
+
+The code contained in this file was copied from the CRAN R package CFFDRS
+(Wang X, Wotton B M, Cantin A, Parisien M-A, Anderson K, Moore B and
+Flannigan M D (2017).
+cffdrs: An R package for the Canadian Forest Fire Danger Rating System.
+Ecological Processes, 6(1), 5.
+URL
+https://ecologicalprocesses.springeropen.com/articles/10.1186/s13717-017-0070-z)
+and translated into dart for FBP Go.
+
+FBP Go is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+FBP Go is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+FBP Go. If not, see <https://www.gnu.org/licenses/>.
+*/
 // ignore_for_file: non_constant_identifier_names
 
 double FROScalc(double ROS, double BROS, double LB) {
--- lib/fire.dart
+++ lib/fire.dart
@@ -1,3 +1,20 @@
+/*
+Copyright 2021, 2022 Province of British Columbia
+
+This file is part of FBP Go.
+
+FBP Go is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+FBP Go is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+FBP Go. If not, see <https://www.gnu.org/licenses/>.
+*/
 import 'dart:math';
 
 import 'cffdrs/dist_calc.dart';
