edbdf2e8b5d609d91ac29567b1b7c92077a49059
Strauss <Sybrand.Strauss@gov.bc.ca>
2022-05-09 16:55:54 -0700

    added some rounding

--- test/cffdrs_test.dart
+++ test/cffdrs_test.dart
@@ -85,7 +85,7 @@ FireBehaviourPredictionPrimary loadOutput(dynamic outputJson) {
 
 double roundDouble(double value) {
   /*Hypothesis A: R has a slightly different way of doing floating point
-  calculations resulting in minor differences in the output. We're unlinkely
+  calculations resulting in minor differences in the output. We're unlikely
   to care about anything beyond 2 decimal places.
   Hypothesis B: There's a mistake in the translation, some detail in operation
   order or some such which is causing slightly different results.
@@ -269,7 +269,8 @@ void main() {
               reason: 'FCFB $i');
           expect(resultSecondary.BCFB, expectedSecondary.BCFB,
               reason: 'BCFB $i');
-          expect(resultSecondary.TCFB, expectedSecondary.TCFB,
+          expect(roundDouble(resultSecondary.TCFB),
+              roundDouble(expectedSecondary.TCFB),
               reason: 'TCFB $i');
           expect(roundDouble(resultSecondary.FTFC),
               roundDouble(expectedSecondary.FTFC),
