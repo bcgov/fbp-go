592f55ac473a7974509f5df30cc495c80025220a
Brett Edwards <brett.1.edwards@gov.bc.ca>
2024-05-02 12:45:14 -0700

    deploy script

--- openshift/README.md
+++ openshift/README.md
@@ -13,16 +13,24 @@ oc -n e1e498-tools process -f build.yaml | oc -n e1e498-tools apply -f -
 ### Apply template using a specified branch and version
 
 ```bash
-oc -n e1e498-tools process -p VERSION=some-date -p GIT_BRANCH=my-branch process -f build.yaml | oc -n e1e498-tools apply -f -
+oc -n e1e498-tools -p VERSION=some-date -p GIT_BRANCH=my-branch process -f openshift/templates/build.yaml | oc -n e1e498-tools apply -f -
 ```
 
 ### Kick off the build
 
 ```bash
-oc -n e1e498-tools start-build flutter-build --follow
+oc -n e1e498-tools start-build fbp-go --follow
 ```
 
-### Re-tag for production
+### Deploy
+
+Deploy using bash script with the "Version" of ImageStreamTag and specifying test/prod as a suffix. 'test' deploys to dev, 'prod' deploys to prod
+
+```bash
+VERSION="may-2-2024" bash openshift/scripts/oc_deploy.sh test apply
+```
+
+### TODO Re-tag for production
 
 Assuming you've built an image tagged for dev, you may now want to tag it for production. Remember to retain
 the current prod image in case you want to revert!
---
+++ openshift/scripts/oc_build.sh
@@ -0,0 +1,66 @@
+#!/bin/sh -l
+#
+source "$(dirname ${0})/common/common"
+
+# %
+# % OpenShift Build Helper
+# %
+# %   Intended for use with a pull request-based pipeline.
+# %   Suffixes incl.: pr-###, test and prod.
+# %
+# % Usage:
+# %
+# %   ${THIS_FILE} [SUFFIX] [apply]
+# %
+# % Examples:
+# %
+# %   Provide a PR number. Defaults to a dry-run.
+# %   ${THIS_FILE} pr-0
+# %
+# %   Apply when satisfied.
+# %   ${THIS_FILE} pr-0 apply
+# %
+OBJ_NAME="${APP_NAME}-${SUFFIX}"
+
+# Process a template (mostly variable substition)
+#​​
+OC_PROCESS="oc -n ${PROJ_TOOLS} process -f ${PATH_BC} \
+ -p SUFFIX=${SUFFIX} \
+ ${DOCKER_FILE:+ "-p DOCKER_FILE=${DOCKER_FILE}"}"
+
+# Apply a template (apply or use --dry-run)
+
+OC_APPLY="oc -n ${PROJ_TOOLS} apply -f -"
+[ "${APPLY}" ] || OC_APPLY="${OC_APPLY} --dry-run=client"
+
+# Cancel non complete builds and start a new build (apply or don't run)
+#
+OC_CANCEL_BUILD="oc -n ${PROJ_TOOLS} cancel-build bc/${OBJ_NAME}"
+[ "${APPLY}" ] || OC_CANCEL_BUILD=""
+OC_START_BUILD="oc -n ${PROJ_TOOLS} start-build ${OBJ_NAME} --follow=true --wait=true"
+[ "${APPLY}" ] || OC_START_BUILD=""
+
+# Execute commands
+#
+eval "${OC_PROCESS}"
+eval "${OC_PROCESS} | ${OC_APPLY}"
+eval "${OC_CANCEL_BUILD}"
+eval "${OC_START_BUILD}"
+
+if [ "${APPLY}" ]; then
+	# Get the most recent build version
+	BUILD_LAST=$(oc -n ${PROJ_TOOLS} get bc/${OBJ_NAME} -o 'jsonpath={.status.lastVersion}')
+	# Command to get the build result
+	BUILD_RESULT=$(oc -n ${PROJ_TOOLS} get build/${OBJ_NAME}-${BUILD_LAST} -o 'jsonpath={.status.phase}')
+
+	# Make sure that result is a successful completion
+	if [ "${BUILD_RESULT}" != "Complete" ]; then
+		echo "Build result: ${BUILD_RESULT}"
+		echo -e "\n*** Build not complete! ***\n"
+		exit 1
+	fi
+fi
+
+# Provide oc command instruction
+#
+display_helper "${OC_PROCESS} | ${OC_APPLY}" "${OC_CANCEL_BUILD}" "${OC_START_BUILD}"
